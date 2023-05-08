df9c59bf5450a95461fd3521ca264bb2eb4f3f4a
Rodrigo Bernel <106604143+roobernel@users.noreply.github.com>
2023-05-07 22:39:50 -0300

    ejercicio2

--- ejercicio2.js
+++ ejercicio2.js
@@ -10,3 +10,45 @@
 // c. Categoría del producto.
 // d. Precio del producto.
 // e. Descripción del producto.
+
+function cargar(){
+    let mostrar=
+    `<input type="number" class="campoform" placeholder="Código de producto" id="codigo" required>
+    <input type="text" class="campoform" placeholder="Nombre de producto" id="nombre" required>
+    <input type="text" class="campoform" placeholder="Categoría de producto" id="categoria" required>
+    <input type="number" class="campoform" placeholder="Precio de producto" id="precio" required>
+    <input type="text" class="campoform" placeholder="Descripción de prodcuto" id="descripcion" required>
+    <input type="submit" onclick="obtener();" value="Cargar productos">`
+    document.getElementById("forms").innerHTML=mostrar;
+}
+let productos=[];
+let item=0
+function obtener(){
+    let codigo=0, categoria='', nombre='', precio=0, descripcion='';
+    codigo=document.getElementById("codigo").value;
+    nombre=document.getElementById("nombre").value;
+    categoria=document.getElementById("categoria").value;
+    precio=document.getElementById("precio").value;
+    descripcion=document.getElementById("descripcion").value;
+    productos[item]=[codigo, nombre, categoria, precio, descripcion];
+    item+=1;
+}
+
+
+function info(){
+    let mostrar='';
+    for(let x in productos){
+        mostrar+=`<div class="producto">
+        <h2> ${productos[x][1]} </h2>
+        <p>Código:${productos[x][0]}</p>
+        <p>Categoría:${productos[x][2]}</p>    
+        <p>Precio:${productos[x][3]}</p>    
+        <p>Descripción:${productos[x][4]}</p>            
+        </div>`;
+    }
+    document.getElementById("items").innerHTML=mostrar;
+}
+
+
+
+    
